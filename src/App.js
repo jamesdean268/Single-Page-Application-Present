22d8db854f73ef97e74e46d74b2f413bc03db61b
James Dean <jamesdean268@gmail.com>
2023-12-23 20:28:33 +1030

    Added basic web page, css, and background image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="voucher-container">
+      <h1 className="voucher-heading">Merry Christmas Nicole!</h1>
+      <div className="voucher-textbox">
+        <p>Voucher for 1x ticket to Miss Saigon in Adelaide in January</p>
+        <a href="https://miss-saigon.com.au/tickets/" className="voucher-link">
+          Click here to purchase tickets
         </a>
-      </header>
+        <p className="voucher-fineprint">
+          * this voucher includes approximately 3x kid free hours where James will spend the day with Sebastian in town (bottle of milk not included).
+        </p>
+      </div>
+      <p className="voucher-sender">Love, Husband (aka Dad) xxx</p>
     </div>
   );
 }
